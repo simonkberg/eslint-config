969fff228cfb9d8a6955642be8660b58bc25c3ac
Simon Kjellberg <simon.kjellberg@gmail.com>
2019-05-27 22:42:10 +0200

    feat(typescript): standardize and relax ts rules

--- typescript.js
+++ typescript.js
@@ -7,4 +7,58 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     require.resolve('eslint-config-prettier/@typescript-eslint'),
   ],
+  rules: {
+    '@typescript-eslint/camelcase': [
+      'error',
+      {
+        properties: 'never',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: false,
+        variables: false,
+        typedefs: false,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // Checked by Typescript - ts(2300)
+        'no-dupe-args': 'off',
+        // Checked by Typescript - ts(1117)
+        'no-dupe-keys': 'off',
+        // Checked by Typescript - ts(7027)
+        'no-unreachable': 'off',
+        // Checked by Typescript - ts(2367)
+        'valid-typeof': 'off',
+        // Checked by Typescript - ts(2588)
+        'no-const-assign': 'off',
+        // Checked by Typescript - ts(2588)
+        'no-new-symbol': 'off',
+        // Checked by Typescript - ts(2376)
+        'no-this-before-super': 'off',
+      },
+    },
+  ],
 }
